480dda36ad002ae1ef82a00487b057950b715ebb
Kalinac <akalincevic@yahoo.com>
2025-02-05 00:09:26 +0100

    Finished

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from product import Product
 from product_manager import ProductManager
 from cart import Cart
+import random
 if __name__ == "__main__":
-    manager = ProductManager()
+   manager = ProductManager()
     
 #Dodavanje proizvoda u listu
-product1 = Product("Televizor", 120000, 3))
-product2 = Product("Frizider", 85000, 2))
-product3 = Product("Ves masina", 70000, 4))
+product1 = Product("Televizor", 120000, 3)
+product2 = Product("Frizider", 85000, 2)
+product3 = Product("Ves masina", 70000, 4)
 product4 = Product("Mikrotalasna", 15000, 5)
 product5 = Product("Usisivac", 25000, 6)
 
