7102069b3bc3d92deee9d7923361b8d4f920b994
Kalinac <akalincevic@yahoo.com>
2025-02-04 23:52:04 +0100

    changed main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from product import Product
 from product_manager import ProductManager
-
 if __name__ == "__main__":
     manager = ProductManager()
     
 #Dodavanje proizvoda
-manager.add_product(Product("Televizor", 120000, 3))
-manager.add_product(Product("Frizider", 85000, 2))
-manager.add_product(Product("Ves masina", 70000, 4))
-
-#Prikaz proizvoda i ukupne vrednosti
-print("\nDostupni proizvodi:")
-manager.display_products()
-
-print("\nUkupna vrednost inventara:")
-manager.total_value()
+manager.add_product(Product("Smart TV", 120000, 3))
+manager.add_product(Product("Frizider", 85000, 5))
+manager.add_product(Product("Blender", 70000, 4))
